eba533d2cc50e693f4c4dbc7e1028c63524f5dfe
Chaoren Lin <chaorenl@google.com>
2015-08-19 14:24:31 -0700

    Get build-protobuf.sh working on Windows and update to 2.6.1.
    Change-Id: I74097a8648daa9b49c423e108659c5e6ca5ef7a1

--- prebuilts/build-protobuf.sh
+++ prebuilts/build-protobuf.sh
@@ -3,13 +3,13 @@
 # https://android.googlesource.com/platform/external/lldb-utils
 #
 # Download & build protobuf on the local machine
-# works on Linux, OS X
-# TODO: get it working on Windows
+# works on Linux, OS X, and Windows (Cygwin)
 # leaves output in /tmp/prebuilts/libprotobuf/$OS-x86
 # requires autoconf, automake, libtool, chrpath
 
 PROJ=libprotobuf
-VER=2.5.0
+VER=2.6.1
+MSVS=2013
 
 source $(dirname "$0")/build-common.sh build-common.sh
 
@@ -21,22 +21,39 @@ curl -L https://github.com/google/protobuf/archive/$TGZ -o $TGZ
 tar xzf $TGZ || cat $TGZ # if this fails, we're probably getting an http error
 cd $BASE
 ./autogen.sh
-mkdir $RD/build
-cd $RD/build
-$RD/$BASE/configure --prefix=$INSTALL
-make -j$CORES
-make install
+
+case "$OS" in
+	windows)
+		cd vsprojects
+		sed -i 's/\(IntermediateDirectory=\)".*"/\1"$(OutDir)$(ProjectName)"/' *.vcproj
+		devenv protobuf.sln /Upgrade
+		devenv protobuf.sln /Build Debug
+		devenv protobuf.sln /Build Release
+		mkdir -p $INSTALL/Debug $INSTALL/Release
+		cp -a Debug/*.* $INSTALL/Debug
+		cp -a Release/*.* $INSTALL/Release
+		cmd /c extract_includes.bat
+		cp -a include $INSTALL/
+		;;
+	linux|darwin)
+		mkdir $RD/build
+		cd $RD/build
+		$RD/$BASE/configure --prefix=$INSTALL
+		make -j$CORES
+		make install
+		;;
+esac
 
 case "$OS" in
 	linux)
-		for TARGET in $INSTALL/{bin/protoc,lib/libprotoc.so.8}; do
+		for TARGET in $INSTALL/{bin/protoc,lib/libprotoc.so}; do
 			chrpath -r '$ORIGIN/../lib' $TARGET
 		done
 		;;
 	darwin)
-		for LIB in lib/libproto{c,buf{,-lite}}.8.dylib; do
+		for LIB in lib/libproto{c,buf{,-lite}}.9.dylib; do
 			install_name_tool -id @executable_path/../$LIB $INSTALL/$LIB
-			for TARGET in $INSTALL/{bin/protoc,lib/libprotoc.8.dylib}; do
+			for TARGET in $INSTALL/{bin/protoc,lib/libprotoc.dylib}; do
 				ABSOLUTE=$INSTALL/$LIB
 				RELATIVE=@executable_path/../$LIB
 				install_name_tool -change $ABSOLUTE $RELATIVE $TARGET
